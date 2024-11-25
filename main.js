951b2013f55638d5dd304dd2eca1d0d8eb7147ef
Saleel <saleel@saleel.xyz>
2024-11-26 01:47:11 +0400

    vite sample

---
+++ main.js
@@ -0,0 +1,31 @@
+import { UltraHonkBackend } from "@aztec/bb.js";
+import { Noir } from "@noir-lang/noir_js";
+import circuit from "../noir-bb-sample/app/assets/circuit.json";
+import vkey from "../noir-bb-sample/app/assets/circuit-vkey.json";
+import initNoirC from '@noir-lang/noirc_abi';
+import initACVM from '@noir-lang/acvm_js';
+
+const generateProof = async () => {
+  await Promise.all([
+    initACVM(new URL('@noir-lang/acvm_js/web/acvm_js_bg.wasm', import.meta.url).toString()),
+    initNoirC(
+      new URL('@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm', import.meta.url).toString(),
+    ),
+  ]);
+
+  const noir = new Noir(circuit);
+
+  const backend = new UltraHonkBackend(circuit.bytecode);
+
+  const startTime = performance.now();
+  const { witness } = await noir.execute({ x: 3, y: 3 });
+  const proof = await backend.generateProof(witness);
+  const provingTime = performance.now() - startTime;
+
+  console.log(`Proof generated in ${provingTime}ms`, proof);
+
+  const verified = await backend.verifyProof(proof);
+  console.log("Proof verified:", verified);
+};
+
+window.generateProof = generateProof;
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import topLevelAwait from "vite-plugin-top-level-await";
+
+export default defineConfig({
+  plugins: [topLevelAwait()],
+});
