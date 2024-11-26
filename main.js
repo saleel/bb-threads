3672046887a0cbfd2716356c273ae73fa2517898
Saleel <saleel@saleel.xyz>
2024-11-26 15:30:07 +0400

    add pages

--- main.js
+++ main.js
@@ -9,12 +9,15 @@ const doc = document.getElementById("proof-result");
 
 function print(text) {
   console.log(text);
-  doc.innerHTML = text;
+  doc.innerHTML += text + "<br/>";
 }
 
 const generateProof = async () => {
   try {
     print("Initializing...");
+
+    const api = await Barretenberg.new();
+    print(`Threads: ${await api.getNumThreads()}`);
 
     await Promise.all([
       initACVM(new URL('@noir-lang/acvm_js/web/acvm_js_bg.wasm', import.meta.url).toString()),
