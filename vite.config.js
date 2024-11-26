7261ee913ff5f8c1b291fa1aebe0e0e6fb47ed9e
Saleel <saleel@saleel.xyz>
2024-11-26 15:40:24 +0400

    update page

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,9 @@ import topLevelAwait from "vite-plugin-top-level-await";
 export default defineConfig({
   plugins: [topLevelAwait()],
   server: {
-    middleware: [
-      (req, res, next) => {
-        console.log(req.url);
-        if (req.url.startsWith('/multi-thread')) {
-          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-        }
-        next();
-      }
-    ]
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
   },
 });
