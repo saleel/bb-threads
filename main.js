000a081fa3c3748d6f2ed719ceed14470102e9fe
Saleel <saleel@saleel.xyz>
2024-11-26 15:09:58 +0400

    add alert

--- main.js
+++ main.js
@@ -6,28 +6,34 @@ import initNoirC from '@noir-lang/noirc_abi';
 import initACVM from '@noir-lang/acvm_js';
 
 const generateProof = async () => {
-  const api = await Barretenberg.new();
-  console.log(await api.getNumThreads());
-  await Promise.all([
-    initACVM(new URL('@noir-lang/acvm_js/web/acvm_js_bg.wasm', import.meta.url).toString()),
-    initNoirC(
-      new URL('@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm', import.meta.url).toString(),
-    ),
-  ]);
+  try {
+    const api = await Barretenberg.new();
+    alert(await api.getNumThreads() + " threads");
 
-  const noir = new Noir(circuit);
+    await Promise.all([
+      initACVM(new URL('@noir-lang/acvm_js/web/acvm_js_bg.wasm', import.meta.url).toString()),
+      initNoirC(
+        new URL('@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm', import.meta.url).toString(),
+      ),
+    ]);
 
-  const backend = new UltraHonkBackend(circuit.bytecode);
+    const noir = new Noir(circuit);
 
-  const startTime = performance.now();
-  const { witness } = await noir.execute({ x: 3, y: 3 });
-  const proof = await backend.generateProof(witness);
-  const provingTime = performance.now() - startTime;
+    const backend = new UltraHonkBackend(circuit.bytecode);
 
-  console.log(`Proof generated in ${provingTime}ms`, proof);
+    const startTime = performance.now();
+    const { witness } = await noir.execute({ x: 3, y: 3 });
+    const proof = await backend.generateProof(witness);
+    const provingTime = performance.now() - startTime;
 
-  const verified = await backend.verifyProof(proof);
-  console.log("Proof verified:", verified);
+    console.log(`Proof generated in ${provingTime}ms`, proof);
+
+    const verified = await backend.verifyProof(proof);
+    console.log("Proof verified:", verified);
+  } catch (e) {
+    console.error(e);
+    alert(e);
+  }
 };
 
 window.generateProof = generateProof;
