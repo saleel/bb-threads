a4f5cb71b8892968937af11caf921100c976f643
saleel <saleel@saleel.xyz>
2025-06-11 18:00:21 +0000

    bb

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,6 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   plugins: [
-    {
-      name: 'buffer-polyfill',
-      transformIndexHtml(html) {
-        return {
-          html,
-          tags: [
-            {
-              tag: 'script',
-              attrs: { type: 'module' },
-              children: `
-                import { Buffer } from 'buffer-es6';
-                window.Buffer = Buffer;
-                globalThis.Buffer = Buffer;
-              `,
-              injectTo: 'head',
-            },
-          ],
-        };
-      },
-    },
     topLevelAwait(),
     nodePolyfills({
       include: ["buffer", "process", "util", "stream", "crypto"],
@@ -46,23 +26,12 @@ export default defineConfig({
       "@noir-lang/acvm_js",
       "@aztec/bb.js",
     ],
-    include: ['buffer-es6'],
   },
   resolve: {
     alias: {
       "pino": "pino/browser",
       "pino/browser": "pino/browser",
       "buffer": "buffer-es6",
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ["pino", "pino/browser"],
-      plugins: [
-        nodePolyfills({
-          include: ["buffer", "process", "util", "stream", "crypto"],
-        }),
-      ],
     },
   },
 });
