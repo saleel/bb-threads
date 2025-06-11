ba36fa737cad53478f35f069b3d91c74235d5ddc
saleel <saleel@saleel.xyz>
2025-06-11 17:26:04 +0000

    add dest

---
+++ dist/assets/circuit-DSKVwO02.js
@@ -0,0 +1,10 @@
+const e="0.39.0+9ae9ef8347ddc608659563050edb6752f5dfb87d",t=6280250985204372e3,i={parameters:[{name:"x",type:{kind:"field"},visibility:"private"},{name:"y",type:{kind:"field"},visibility:"public"}],return_type:{abi_type:{kind:"field"},visibility:"public"},error_types:{}},s="H4sIAAAAAAAA/62QQQqAMAwEjfigpElscvMrFtv/P0HECqUHL+1AWMhh2V1YXtZ6Pd/vqIpjUOvFuIvkGDIxnRg8maJo2o2M1PQKxpxNLHryiE7CmYo6l2oG83LhX+fRnDP3Wyd2fvbbqhc0/aFR6DZpuQERQ5xYPAIAAA==",a="TYxLCsMwDAXvonUWXbSl9VVKCf7IQWBkI9uFYnL3KqGB7DRv0AwI6PoyE8dcwbwGpOxto8xKAy77VIvljWqz0sDcHhMgBzD36zpBpIR6Pte3ghNKiZb5HNH5Y4WsS/jH2NmfbPuWwxz/RbLH0AW30u40/wM=",b={69:{source:`fn main(x: Field, y: pub Field) -> pub Field {
+    assert(x == y);
+    y
+}
+
+#[export]
+fn exported() -> Field {
+    3
+}
+`,path:"/Users/saleel/Aztec/noir-bb-sample/circuits/src/main.nr"}},l=["main"],n=[],r={noir_version:e,hash:6280250985204372e3,abi:i,bytecode:s,debug_symbols:a,file_map:b,names:l,brillig_names:n};export{i as abi,n as brillig_names,s as bytecode,a as debug_symbols,r as default,b as file_map,t as hash,l as names,e as noir_version};
